e599f619dbf800a070fe271e794655472c168d4d
Andrei Popov <andrepopoff@gmail.com>
2020-08-01 12:05:33 +0300

    Add the second version of singleton without a global variable

---
+++ singleton_2.js
@@ -0,0 +1,27 @@
+class Counter {
+    constructor() {
+        if (typeof Counter.instance === 'object') {
+            return Counter.instance
+        }
+        this.count = 0;
+        Counter.instance = this;
+        return this
+    }
+    getCount() {
+        return this.count
+    }
+    increaseCount() {
+        this.count++
+    }
+}
+
+const counter1 = new Counter();
+const counter2 = new Counter();
+
+counter1.increaseCount();
+counter1.increaseCount();
+counter2.increaseCount();
+counter2.increaseCount();
+
+console.log(counter1.getCount()); // 4
+console.log(counter2.getCount()); // 4
